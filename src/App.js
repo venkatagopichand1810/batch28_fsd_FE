e64f2327cbd2a7782da702678b2e9cac5b9e1a56
venkatguvi <venkatagopichand9@gmail.com>
2025-11-30 20:24:42 +0530

    added the FE changes

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import StudentForm from "./components/StudentForm"
+import "./App.css"
+
+function App() {
+
+  return (
+    <div className="App">
+      <h1>Guvi Student Management System</h1>
+      <StudentForm />
+    </div>
+  )
+
+}
+
+export default App
---
+++ src/components/StudentForm.js
@@ -0,0 +1,93 @@
+import { useState } from "react"
+import "./StudentForm.css"
+
+function StudentForm() {
+    // create the state variable
+    const [student, setStudent] = useState({
+        name: "",
+        age: "",
+        course: ""
+    })
+
+    // to handle the values in the input field we have to create hanldeChange function 
+
+    const handleChange = (e) => {
+        const {name, value} = e.target;
+        setStudent((prevValue) => ({
+            ...prevValue, 
+            [name]: value
+        }))
+    }
+
+    // a function to handle the form submission 
+    const handleSubmit = async (e) => {
+        e.preventDefault(); //prevent the defualt behaviour of the form submission
+
+        try {
+            const res = await fetch("http://localhost:5004/students", {
+                method: "POST",
+                headers: {
+                    "Content-Type" : "application/json"
+                },
+                body: JSON.stringify(student)
+            });
+
+            if(res.ok){
+                const data = await res.json(); 
+
+                // clear form
+                setStudent({
+                    name: "",
+                    age: "",
+                    course: ""
+                })
+            } else {
+                console.log("faild to submit")
+            }
+
+        } catch(error){
+            console.log("error")
+        }
+    }
+
+    return (
+        <>
+           <form onSubmit={handleSubmit} className="student-form">
+            
+            <input 
+                type="text"
+                placeholder="Please enter your name"
+                value={student.name}
+                name = "name"
+                required
+                onChange={handleChange}
+            />
+
+             <input 
+                type="number"
+                placeholder="Please enter your Age"
+                value={student.age}
+                name = "age"
+                required
+                onChange={handleChange}
+            />
+
+
+             <input 
+                type="text"
+                placeholder="Please enter your course name"
+                value={student.course}
+                name = "course"
+                required
+                onChange={handleChange}
+            />
+
+            <button type="submit">Submit</button>
+
+           </form>
+        </>
+    )
+
+}
+
+export default StudentForm
